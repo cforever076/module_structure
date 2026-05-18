4284eccde6f3e935ba0cf8809aba97c477f47526
prakash <prakashs4u@gmail.com>
2026-05-18 15:27:38 +0530

    new output.tf added

--- dev/output.tf
+++ dev/output.tf
@@ -1,14 +1,11 @@
 output "rg_name" {
-    value = module.rgs.rg_name
-  
+value = module.rgs.rg_name
 }
 
 output "rg_location" {
-    value = module.rgs.rg_location
-  
+value = module.rgs.rg_location
 }
 
 output "storage_account_name" {
-    value = module.storage_account.storage 
-  
+value = module.storage_account.storage 
 }
